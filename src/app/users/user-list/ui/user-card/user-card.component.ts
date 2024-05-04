9c5f4c3c654047e4940aa5ee36f204aae829997d
Rocard Fonji <fonkengrocard@gmail.com>
2024-05-04 13:39:52 +0100

    (feature: user-details): create user detail component and setup routing

--- src/app/users/user-list/ui/user-card/user-card.component.ts
+++ src/app/users/user-list/ui/user-card/user-card.component.ts
@@ -1,12 +1,14 @@
 import { Component, Input } from '@angular/core';
 import { User } from '../../../../shared/interfaces/user';
 import { MatCardModule } from '@angular/material/card';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-user-card',
   standalone: true,
   imports: [
-    MatCardModule
+    MatCardModule,
+    RouterLink
   ],
   templateUrl: './user-card.component.html',
   styleUrl: './user-card.component.scss'
--- src/app/users/users.routes.ts
+++ src/app/users/users.routes.ts
@@ -6,4 +6,9 @@ export const USERS_ROUTES: Route[] = [
     loadComponent: () =>
       import('./user-list/list/list.component').then((comp) => comp.ListComponent),
   },
+  {
+    path: ':id',
+    loadComponent: () =>
+      import('./user-detail/detail/detail.component').then((comp) => comp.DetailComponent),
+  }
 ];
