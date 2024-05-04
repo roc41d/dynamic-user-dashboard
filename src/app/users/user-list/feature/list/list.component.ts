20eaf090efb1f842d18c2fb09c10ed4171472ecb
Rocard Fonji <fonkengrocard@gmail.com>
2024-05-04 12:07:15 +0100

    (feature: list-users): add logic to get users from store and ui to disolay users data

--- src/app/users/user-list/feature/list/list.component.ts
+++ src/app/users/user-list/feature/list/list.component.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { userActions } from '../../data-access/store/actions';
+import { combineLatest } from 'rxjs';
+import { getUsers, selectError, selectLoading, selectTotal } from '../../data-access/store/reducers';
+import { AsyncPipe } from '@angular/common';
+import {MatCardModule} from '@angular/material/card';
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [],
+  imports: [
+    AsyncPipe,
+    MatCardModule
+  ],
   templateUrl: './list.component.html',
   styleUrl: './list.component.scss'
 })
-export class ListComponent {
+export class ListComponent implements OnInit{
+  private currentPage = 1;
+  private store = inject(Store);
 
+  data$ = combineLatest({
+    isLoading: this.store.select(selectLoading),
+    users: this.store.select(getUsers),
+    error: this.store.select(selectError),
+    total: this.store.select(selectTotal),
+  })
+
+  ngOnInit(): void {
+    this.loadUsers(this.currentPage);
+  }
+
+  loadUsers(page: number): void {
+    this.store.dispatch(userActions.getUsers({ page }));
+  }
 }
