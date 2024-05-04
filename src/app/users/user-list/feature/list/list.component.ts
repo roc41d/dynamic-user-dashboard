7e8146b9e46fdb45aa921ea04e0225324cae88be
Rocard Fonji <fonkengrocard@gmail.com>
2024-05-04 12:15:11 +0100

    chore: create loader component

--- src/app/users/user-list/feature/list/list.component.ts
+++ src/app/users/user-list/feature/list/list.component.ts
@@ -5,13 +5,15 @@ import { combineLatest } from 'rxjs';
 import { getUsers, selectError, selectLoading, selectTotal } from '../../data-access/store/reducers';
 import { AsyncPipe } from '@angular/common';
 import {MatCardModule} from '@angular/material/card';
+import { LoaderComponent } from '../../../../shared/ui/loader.component';
 
 @Component({
   selector: 'app-list',
   standalone: true,
   imports: [
     AsyncPipe,
-    MatCardModule
+    MatCardModule,
+    LoaderComponent
   ],
   templateUrl: './list.component.html',
   styleUrl: './list.component.scss'
